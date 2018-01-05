8cadeba0ff69f0f0d17c46a61fa777a3810a659d
Theriley106 <christopherlambert106@gmail.com>
2018-01-04 19:41:59 -0500

    Removed grabsite

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ import bs4
 import csv
 
 
-def grabSite(url):
-	headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-	return requests.get(url, headers=headers)
 
 class spinxAPI(object):
 	#placeholder bot class - will eventually merge a ton of stuff into this
