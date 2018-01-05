6dab76cda0b5a6f0378ee7e5e7b53bf4a50f9df9
Theriley106 <christopherlambert106@gmail.com>
2018-01-04 19:36:12 -0500

    added ability to get pricing

---
+++ main.py
@@ -0,0 +1,42 @@
+import requests
+import bs4
+import csv
+
+
+def grabSite(url):
+	headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+	return requests.get(url, headers=headers)
+
+class spinxAPI(object):
+	#placeholder bot class - will eventually merge a ton of stuff into this
+	def __init__(self, defaultStore=None):
+		self.updateDatapoints()
+
+	def updateDatapoints(self):
+		self.Datapoints = []
+		res = requests.get('http://zingon-data.com/spinx/prices.csv')
+		decoded_content = res.content.decode('utf-8')
+		cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+		my_list = list(cr)
+		for row in my_list:
+			newVal = []
+			for val in row:
+				if len(val.strip()) > 2:
+					newVal.append(val.strip())
+			if len(newVal) > 1:
+				self.Datapoints.append(newVal)
+
+	def displayPricing(self):
+		if len(self.Datapoints) == 0:
+			self.updateDatapoints()
+		else:
+			for val in self.Datapoints:
+				print val
+
+
+
+
+if __name__ == '__main__':
+	a = spinxAPI()
+	a.displayPricing()
+
