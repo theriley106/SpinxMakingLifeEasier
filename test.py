ec7157019382cdf61e414bfdc3a23d1c0601ffc2
Theriley106 <christopherlambert106@gmail.com>
2018-01-06 22:31:50 -0500

    removed pyc file

---
+++ test.py
@@ -0,0 +1,12 @@
+import requests
+
+headers = {
+    'Content-type': 'application/json',
+    'access_token': 'f1b412b8-0d5e-4164-8045-d3cfc24796c2',
+    'Host': 'restmws.fuelrewards.com',
+    'User-Agent': 'Apache-HttpClient/UNAVAILABLE (java 1.4)',
+}
+
+response = requests.get('https://restmws.fuelrewards.com/fuelrewards/public/rest/cardbalance/cardnumber/782424421400826845/participantid/24270', headers=headers)
+
+print response.json()
