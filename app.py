1f438c43eb59e6bc4856c1eaf18afe705e4afc80
Theriley106 <christopherlambert106@gmail.com>
2018-05-06 18:34:25 -0400

    Added flask app

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, url_for, redirect, Markup, jsonify, make_response, send_from_directory, session
+
+app = Flask(__name__, static_url_path='/static')
+
+
+@app.route('/', methods=['GET'])
+def index():
+	price = {"Dollar": '1', "CentOne": '1', "CentTwo": '2'}
+	return render_template("flipClock.html", price=price)
+
+@app.route('/test', methods=['GET'])
+def testPage():
+	return render_template("index1.html")
+
+if __name__ == '__main__':
+	app.run(host='127.0.0.1', port=5000)
