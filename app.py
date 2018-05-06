fa37611d24a90f3ca2528798f1a041230a6dbb21
Theriley106 <christopherlambert106@gmail.com>
2018-05-06 19:05:02 -0400

    Added divs containing pricing info

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ app = Flask(__name__, static_url_path='/static')
 @app.route('/', methods=['GET'])
 def index():
 	price = {"Dollar": '1', "CentOne": '1', "CentTwo": '2'}
-	return render_template("index.html", price=4.15)
+	store = {"Address": '4 address', "Price": 5.11, "Number": 2261}
+	return render_template("index.html", store=store)
 
 @app.route('/test', methods=['GET'])
 def testPage():
