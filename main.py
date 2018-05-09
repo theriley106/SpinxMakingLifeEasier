d43809b8eaf9088c604da7fb0ecb2245d683d310
Theriley106 <christopherlambert106@gmail.com>
2018-05-09 15:24:26 -0400

    Removed 'test' store - number: 9128

--- main.py
+++ main.py
@@ -38,16 +38,17 @@ class spinxAPI(object):
 	def DB(self):
 		DB = []
 		for key, val in self.Data.items():
-			info = {}
-			info["Number"] = key
-			info["Options"] = []
-			for e, z in self.Data[key].items():
-				info["Options"].append({"Name": e, "Price": float(z)})
-			DB.append(info)
-			info["Price"] = "Error"
-			for val in info["Options"]:
-				if val['Name'] == 'Unleaded 87':
-					info["Price"] = val["Price"]
+			if str(key) != '9128':
+				info = {}
+				info["Number"] = key
+				info["Options"] = []
+				for e, z in self.Data[key].items():
+					info["Options"].append({"Name": e, "Price": float(z)})
+				DB.append(info)
+				info["Price"] = "Error"
+				for val in info["Options"]:
+					if val['Name'] == 'Unleaded 87':
+						info["Price"] = val["Price"]
 		return DB
 
 
